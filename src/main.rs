6640f761d2a104e01d36180e6ebf7fd37e8e239b
Ryan Chaput <ryanchaput5@gmail.com>
2023-12-19 16:33:47 -0500

    Placeholder stuff in main

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,16 @@ mod player;
 
 fn main() {
     println!("Welcome to Blackjack!");
+
+    let pcard1: u32 = 0; //Placeholders
+    let pcard2: u32 = 0;
+
+    let psum = pcard1 + pcard2;
+
+    let dcard1: u32 = 0;
+    let dcard2: u32 = 0;
+
+    println!("Your cards are: {pcard1}, {pcard2}, for a total value of: {psum}");
+
+    println!("The dealer's cards are: {dcard1}, {dcard2}")
 }
